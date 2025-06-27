6c2e1aad007df149c23f69746d3dc36201d871d4
radityaaldri <muhammadradityaaldri@gmail.com>
2025-06-27 12:27:52 +0700

    Add files via upload

---
+++ tugas_pbo_atm/Tugas_PBO_ATM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tugas_pbo_atm;
+
+/**
+ *
+ * @author Acer
+ */
+public class Tugas_PBO_ATM {
+
+    public static void main(String[] args) {
+        System.out.println("HAI SEMUANYA!!");
+    }
+}
